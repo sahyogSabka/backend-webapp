d2b5ccf7d1facd1a568b71c9fc983b8a8e390761
Himanshu Sharma <hishrma00@gmail.com>
2024-06-01 13:29:01 +0530

    add-fooitem-api

--- src/controllers/restaurant.js
+++ src/controllers/restaurant.js
@@ -1,4 +1,18 @@
 const Restaurant = require('../models/restaurant')
+const FoodItem = require('../models/fooditem')
+const mongoose = require('mongoose');
+
+// Accessing ObjectId from Mongoose
+const ObjectId = mongoose.Types.ObjectId;
+
+// Function to safely create ObjectId from string
+function createObjectId(id) {
+    if (ObjectId.isValid(id)) {
+        return new ObjectId(id);
+    } else {
+        throw new Error('Invalid ObjectId format');
+    }
+}
 
 async function getAllRestaurants(req, res) {
   try {
@@ -9,4 +23,14 @@ async function getAllRestaurants(req, res) {
   }
 }
 
-module.exports = { getAllRestaurants };
+async function getFooditemsByRestaurantId(req, res) {
+  try {
+    const fooditemObjectid = createObjectId(req.params.id)
+    let data = await FoodItem.find({'restaurant.id' : fooditemObjectid})
+    res.send({success: true, data})
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+module.exports = { getAllRestaurants, getFooditemsByRestaurantId };
