eebbd7a75dc099735839238b29de901dad9a4b97
Himanshu Sharma <hishrma00@gmail.com>
2024-07-17 17:19:12 +0530

    wip

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const bcrypt = require('bcryptjs');
 
 // Define the User schema
 const userSchema = new mongoose.Schema({
@@ -9,7 +8,6 @@ const userSchema = new mongoose.Schema({
   },
   mobile: {
     type: String,
-    // required: true,
   },
   type: {
     id: {
@@ -29,21 +27,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  orders: {
+    type: Array,
+  },
 });
-
-// Encrypt password before saving
-// userSchema.pre("save", async function (next) {
-//   if (!this.isModified("password")) {
-//     return next();
-//   }
-//   const salt = await bcrypt.genSalt(10);
-//   this.password = await bcrypt.hash(this.password, salt);
-//   next();
-// });
-
-// userSchema.methods.matchPassword = async function (enteredPassword) {
-//   return await bcrypt.compare(enteredPassword, this.password);
-// };
 
 // Define the User model
 const User = mongoose.model("User", userSchema, "users");
