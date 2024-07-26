7b441e5a1fbab90262ba7ef245b93e0e9afb22d2
Himanshu Sharma <hishrma00@gmail.com>
2024-07-26 22:41:57 +0530

    wip

--- src/controllers/orders.js
+++ src/controllers/orders.js
@@ -3,6 +3,7 @@ const { paiseToRupee } = require("../utils/paiseToRupee");
 const { createObjectId } = require("../utils/createObjectId");
 const { updateUser } = require("../controllers/users");
 const OrderSchema = require("../models/order");
+const Mailer = require("../utils/mailer");
 
 async function makePayment(req, res) {
   // if the amount to be charged is ₹299.00, then pass 29900 means 29900 paise
@@ -51,16 +52,70 @@ async function addOrder({ userId, paymentId, amount, items }) {
   }
 }
 
+async function sendMailOfNewlyCreatedOrder(data) {
+  try {
+    let htmlbody = `<div>
+    There is a new Order.
+    </div>
+    <br/>
+    <strong>
+    <table>
+      <tr>
+        <td>Order id</td>
+        <td>${data.orderId}</td>
+      </tr>
+      <tr>
+        <td>Amount</td>
+        <td>${data.amount}</td>
+      </tr>
+      <tr>
+        <td>User id</td>
+        <td>${data.userId}</td>
+      </tr>
+      <tr>
+        <td>Name</td>
+        <td>${data.userName}</td>
+      </tr>
+      <tr>
+        <td>Mobile</td>
+        <td>${data.mobile}</td>
+      </tr>
+      <tr>
+        <td>Items</td>
+        <td>${JSON.stringify(data.items)}</td>
+      </tr>
+    </table>
+    </strong>`;
+    let subject = `There is a new order`;
+
+    await Mailer(htmlbody, subject);
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
 async function createOrder(req, res) {
   let { orderId, paymentId, userId, amount, name, mobile, orderData } =
     req.body;
+    // console.log('orderId ----------------------->> ',orderId);
+    // console.log('----------------------- ',req.body.orderData);
+    // return {}
   try {
+    // Save the order to the database
     let createdOrder = await addOrder({
       userId,
       paymentId,
       amount,
       items: orderData,
     });
+
+    console.log('{ orderId: createdOrder._id, userId: createdOrder.userId, userName: name, mobile: mobile, paymentId: createdOrder.paymentId, amount: createdOrder.amount, items: createdOrder.items } ---------------------- ',JSON.stringify({ orderId: createdOrder._id, userId: createdOrder.userId, userName: name, mobile: mobile, paymentId: createdOrder.paymentId, amount: createdOrder.amount, items: createdOrder.items }));
+
+    if (createdOrder._id) {
+      await sendMailOfNewlyCreatedOrder({ orderId: createdOrder._id, userId: createdOrder.userId, userName: name, mobile: mobile, paymentId: createdOrder.paymentId, amount: createdOrder.amount, items: createdOrder.items })
+    }
+
+
     let userUpdated = await updateUser(userId, {
       name,
       mobile,
